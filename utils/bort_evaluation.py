458c55f9daadeba2bb4dd9be36554072eb710b3e
Jimin Sohn <estelle26598@gm.gist.ac.kr>
2024-10-02 14:05:23 +0900

    Add bort_evaluation.py

--- utils/bort_evaluation.py
+++ utils/bort_evaluation.py
@@ -9,20 +9,18 @@ import evaluate
 
 def bort_eval(args, eval_dataset, model, device):
     """ Evaluates the given model on the given dataset. """
-    # f1_metric = evaluate.load("f1")
-    f1_metric = evaluate.load("/jimin/huggingface/modules/evaluate_modules/metrics/evaluate-metric--f1/0ca73f6cf92ef5a268320c697f7b940d1030f8471714bffdb6856c641b818974/f1.py")
-
+    f1_metric = evaluate.load("f1")
+    
     # Note that DistributedSampler samples randomly
     eval_sampler = SequentialSampler(eval_dataset)
     eval_dataloader = DataLoader(
         eval_dataset,
         sampler=eval_sampler,
         batch_size=args.eval_batch_size)
-    # tokenizer = AutoTokenizer.from_pretrained("palat/bort")
-    tokenizer = AutoTokenizer.from_pretrained("/jimin/huggingface/hub/models--palat--bort/snapshots/4d76e3ebf80e967148fd5ac81dc364e470083d76", local_files_only=True)
+    tokenizer = AutoTokenizer.from_pretrained("palat/bort")
     cross_ent = torch.nn.CrossEntropyLoss(ignore_index=-100)
 
-    # Evaluate!
+    # Evaluation
     logging.info("***** Running evaluation *****")
     logging.info("  Num examples = %d", len(eval_dataset))
     logging.info("  Batch size = %d", args.eval_batch_size)
@@ -33,29 +31,27 @@ def bort_eval(args, eval_dataset, model, device):
     model.eval()
     out_label_list, preds_list = [], []
     for batch in tqdm.tqdm(eval_dataloader, desc="Evaluating"):
-        orig_epi_token_inputs = tokenizer(list(batch[1]), return_tensors="pt", padding='max_length',
+        orig_epi_token_inputs = tokenizer(list(batch[0]), return_tensors="pt", padding='max_length',
                                           truncation=True, max_length=args.max_seq_len)
 
         with torch.no_grad():
-            epi_token_inputs = orig_epi_token_inputs['input_ids'].to(device) # (b_s, max_seq)
+            epi_token_inputs = orig_epi_token_inputs['input_ids'].to(device)
             epi_attn_mask = orig_epi_token_inputs['attention_mask'].to(device)
 
             outputs = model(epi_token_inputs, attn_mask=epi_attn_mask)
-            # outputs.feature: tensor (b_s, max_seq, 768), outputs.logits: (b_s, max_seq, 7)
-            logits = outputs['logits']  # logits: (b_s, max_seq, 7)
-            label = batch[3].to(device)  # (batch_size, max_seq_len)
-            tmp_eval_loss = cross_ent(logits.flatten(0, 1), label.flatten())  # (64, 256, 7), (64, 256)
+            logits = outputs['logits']
+            label = batch[1].to(device)
+            tmp_eval_loss = cross_ent(logits.flatten(0, 1), label.flatten())
 
             eval_loss += tmp_eval_loss.item()
         nb_eval_steps += 1
         preds = logits.detach().cpu().numpy()
-        preds = np.argmax(preds, axis=2) # (128, 256)
-        out_label_ids = batch[3].detach().cpu().numpy() # (128, 256)
-        # id2ner = {0:'O', 1:'B-PER', 2:'I-PER', 3:'B-ORG', 4:'I-ORG', 5:'B-LOC', 6:'I-LOC'}
+        preds = np.argmax(preds, axis=2)
+        out_label_ids = batch[1].detach().cpu().numpy()
 
         for i in range(out_label_ids.shape[0]):
             for j in range(out_label_ids.shape[1]):
-                if out_label_ids[i, j] != -100:  # 0 is pad_token_id
+                if out_label_ids[i, j] != -100:
                     out_label_list.append(out_label_ids[i, j])
                     preds_list.append(preds[i][j])
 
