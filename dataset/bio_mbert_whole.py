7dc2f2e2dde6062f2666d559967b5e14a9538de8
Jimin Sohn <estelle26598@gm.gist.ac.kr>
2024-10-02 13:50:23 +0900

    Add bio_mbert_whole.py

--- dataset/bio_mbert_whole.py
+++ dataset/bio_mbert_whole.py
@@ -7,114 +7,82 @@ import json
 class BIOMbertWholeDataset(Dataset):
     def __init__(self, lang, split, max_seq_len, ratio=100):
         super(BIOMbertWholeDataset, self).__init__()
+        # High-resource languages
         if lang == 'english':
-            self.file = json.load(
-                open('./data/real_english_wikiann.json'))
+            self.file = json.load(open('./data/english_wikiann.json'))
         elif lang == 'spanish':
-            self.file = json.load(
-                open('./data/real_spanish_wikiann.json'))
+            self.file = json.load(open('./data/spanish_wikiann.json'))
         elif lang == 'korean':
-            self.file = json.load(
-                open('./data/real_korean_wikiann.json'))
-        # CASE 1
+            self.file = json.load(open('./data/korean_wikiann.json'))
+        # CASE 1 - 8 languages
         elif lang == 'maori':
-            self.file = json.load(
-                open('./data/real_maori_wikiann.json'))
+            self.file = json.load(open('./data/maori_wikiann.json'))
         elif lang == 'somali':
-            self.file = json.load(
-                open('./data/real_somali_wikiann.json'))
+            self.file = json.load(open('./data/somali_wikiann.json'))
         elif lang == 'uyghur':
-            self.file = json.load(
-                open('./data/real_uyghur_wikiann.json'))
+            self.file = json.load(open('./data/uyghur_wikiann.json'))
         elif lang == 'sinhala':
-            self.file = json.load(
-                open('./data/real_sinhala_wikiann.json'))
+            self.file = json.load(open('./data/sinhala_wikiann.json'))
         elif lang == 'quechua':
-            self.file = json.load(
-                open('./data/real_quechua_wikiann.json'))
+            self.file = json.load(open('./data/quechua_wikiann.json'))
         elif lang == 'assyrian':
-            self.file = json.load(
-                open('./data/real_assyrian_wikiann.json'))
-        elif lang == 'kashubian':
-            self.file = json.load(
-                open('./data/real_kashubian_wikiann.json'))
+            self.file = json.load(open('./data/assyrian_wikiann.json'))
         elif lang == 'ilocano':
-            self.file = json.load(
-                open('./data/real_ilocano_wikiann.json'))
-        elif lang == 'kyrgyz':
-            # self.file = json.load(open('./data/real_kyrgyz_wikiann.json'))
-            self.file = json.load(open('./data/final_kyrgyz_wikiann.json'))
+            self.file = json.load(open('./data/ilocano_wikiann.json'))
         elif lang == 'kinyarwanda':
-            self.file = json.load(
-                open('./data/real_kinyarwanda_wikiann.json'))
-        # CASE 2
+            self.file = json.load(open('./data/kinyarwanda_wikiann.json'))
+        # CASE 2 - 12 languages
         elif lang == 'esperanto':
-            self.file = json.load(
-                open('./data/real_esperanto_wikiann.json'))
+            self.file = json.load(open('./data/esperanto_wikiann.json'))
         elif lang == 'khmer':
-            self.file = json.load(
-                open('./data/real_khmer_wikiann.json'))
+            self.file = json.load(open('./data/khmer_wikiann.json'))
         elif lang == 'turkmen':
-            self.file = json.load(
-                open('./data/real_turkmen_wikiann.json'))
+            self.file = json.load(open('./data/turkmen_wikiann.json'))
         elif lang == 'amharic':
-            self.file = json.load(
-                open('./data/real_amharic_wikiann.json'))
+            self.file = json.load(open('./data/amharic_wikiann.json'))
         elif lang == 'maltese':
-            self.file = json.load(
-                open('./data/real_maltese_wikiann.json'))
-        # CASE 3
-        elif lang == 'tajik':
-            self.file = json.load(
-                open('./data/real_tajik_wikiann.json'))
-        elif lang == 'yoruba':
-            self.file = json.load(
-                open('./data/real_yoruba_wikiann.json'))
-        elif lang == 'marathi':
-            self.file = json.load(
-                open('./data/real_marathi_wikiann.json'))
-        elif lang == 'javanese':
-            self.file = json.load(
-                open('./data/real_javanese_wikiann.json'))
-        elif lang == 'urdu':
-            self.file = json.load(
-                open('./data/real_urdu_wikiann.json'))
-        elif lang == 'malay':
-            self.file = json.load(
-                open('./data/real_malay_wikiann.json'))
-        elif lang == 'cebuano':
-            self.file = json.load(
-                open('./data/real_cebuano_wikiann.json'))
-        elif lang == 'croatian':
-            self.file = json.load(
-                open('./data/real_croatian_wikiann.json'))
-        elif lang == 'malayalam':
-            self.file = json.load(
-                open('./data/real_malayalam_wikiann.json'))
-        elif lang == 'telugu':
-            self.file = json.load(
-                open('./data/real_telugu_wikiann.json'))
-        elif lang == 'uzbek':
-            self.file = json.load(
-                open('./data/real_uzbek_wikiann.json'))
-        elif lang == 'punjabi':
-            self.file = json.load(
-                open('./data/real_punjabi_wikiann.json'))
-        # Additional
+            self.file = json.load(open('./data/maltese_wikiann.json'))
         elif lang == 'oriya':
-            self.file = json.load(open('./data/t2ps_oriya_wikiann.json'))
-        elif lang == 'belarusian':
-            self.file = json.load(open('./data/t2ps_belarusian_wikiann.json'))
-        elif lang == 'kurdish':
-            self.file = json.load(open('./data/t2ps_kurdish_wikiann.json'))
+            self.file = json.load(open('./data/oriya_wikiann.json'))
         elif lang == 'sanskrit':
-            self.file = json.load(open('./data/t2ps_sanskrit_wikiann.json'))
+            self.file = json.load(open('./data/sanskrit_wikiann.json'))
         elif lang == 'interlingua':
-            self.file = json.load(open('./data/t2ps_interlingua_wikiann.json'))
+            self.file = json.load(open('./data/interlingua_wikiann.json'))
         elif lang == 'guarani':
-            self.file = json.load(open('./data/t2ps_guarani_wikiann.json'))
+            self.file = json.load(open('./data/guarani_wikiann.json'))
+        elif lang == 'belarusian':
+            self.file = json.load(open('./data/belarusian_wikiann.json'))
+        elif lang == 'kurdish':
+            self.file = json.load(open('./data/kurdish_wikiann.json'))
         elif lang == 'sindhi':
-            self.file = json.load(open('./data/t2ps_sindhi_wikiann.json'))
+            self.file = json.load(open('./data/sindhi_wikiann.json'))
+        # CASE 3 - 13 languages
+        elif lang == 'tajik':
+            self.file = json.load(open('./data/tajik_wikiann.json'))
+        elif lang == 'yoruba':
+            self.file = json.load(open('./data/yoruba_wikiann.json'))
+        elif lang == 'marathi':
+            self.file = json.load(open('./data/marathi_wikiann.json'))
+        elif lang == 'javanese':
+            self.file = json.load(open('./data/javanese_wikiann.json'))
+        elif lang == 'urdu':
+            self.file = json.load(open('./data/urdu_wikiann.json'))
+        elif lang == 'malay':
+            self.file = json.load(open('./data/malay_wikiann.json'))
+        elif lang == 'cebuano':
+            self.file = json.load(open('./data/cebuano_wikiann.json'))
+        elif lang == 'croatian':
+            self.file = json.load(open('./data/croatian_wikiann.json'))
+        elif lang == 'malayalam':
+            self.file = json.load(open('./data/malayalam_wikiann.json'))
+        elif lang == 'telugu':
+            self.file = json.load(open('./data/telugu_wikiann.json'))
+        elif lang == 'uzbek':
+            self.file = json.load(open('./data/uzbek_wikiann.json'))
+        elif lang == 'punjabi':
+            self.file = json.load(open('./data/punjabi_wikiann.json'))
+        elif lang == 'kyrgyz':
+            self.file = json.load(open('./data/kyrgyz_wikiann.json'))
         else:
             print(f"Language {lang} is not defined!")
             raise NotImplementedError
@@ -139,8 +107,7 @@ class BIOMbertWholeDataset(Dataset):
             print("Dataset should be train or validation or test!")
             raise NotImplementedError
         self.tokens_list = [" ".join(ele['tokens']) for ele in self.data]
-        # self.tokenizer = BertTokenizer.from_pretrained('google-bert/bert-base-multilingual-cased')
-        self.tokenizer = BertTokenizer.from_pretrained("/jimin/huggingface/hub/models--google-bert--bert-base-multilingual-cased/snapshots/3f076fdb1ab68d5b2880cb87a0886f315b8146f8", local_files_only=True)
+        self.tokenizer = BertTokenizer.from_pretrained('google-bert/bert-base-multilingual-cased')
         self.pad_token = self.tokenizer.pad_token
         self.start_token = self.tokenizer.bos_token
         self.end_token = self.tokenizer.eos_token
@@ -153,11 +120,6 @@ class BIOMbertWholeDataset(Dataset):
         orig_tokens = cur_data['tokens']
         orig_ner_tags = cur_data['ner_tags']
         tokens, token_ner_tags = [], []
-
-        # orig_tokens = ['John', 'works', 'for', 'Globex', 'Corp.', '.']
-        # orig_ner_tags = [1, 0, 0, 3, 4, 0]
-        # orig_tokens = ['John', 'works', 'for', 'Globex', 'Corp.', 'with', 'Paul', '.']
-        # orig_ner_tags = [1,0,0,3,4,0,1,0]
 
         ##### original token pre-process #####
         for i, ele in enumerate(orig_tokens):
@@ -181,17 +143,4 @@ class BIOMbertWholeDataset(Dataset):
         elif self.max_seq_len < len(token_ner_tags):
             token_ner_tags = token_ner_tags[:self.max_seq_len]
 
-        return " ".join(orig_tokens), " ".join(orig_tokens), torch.tensor(token_ner_tags), torch.tensor(token_ner_tags)
-
-if __name__ == '__main__':
-    dataset = BIOMbertWholeDataset('english', 'validation', 128)
-    tmp = next(iter(dataset)) # sample data
-    # a: list 128, b: list 128, c: tensor (128,), d: tensor (128,)
-
-    conll_loader = DataLoader(dataset, batch_size=10, shuffle=True)
-    tmp2 = next(iter(conll_loader))
-
-    tokenizer = BertTokenizer.from_pretrained('google-bert/bert-base-multilingual-cased', padding=True, max_length=128, truncation=True)
-    input_ids = tokenizer(' '.join(tmp2[0]), return_tensors="pt")
-
-    print()
+        return " ".join(orig_tokens), torch.tensor(token_ner_tags)
