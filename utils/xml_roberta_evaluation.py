f8676290d43777a37c9457a770064882f1dbf559
Jimin Sohn <estelle26598@gm.gist.ac.kr>
2024-10-02 14:11:15 +0900

    Add xml_roberta_evaluation.py

--- utils/xml_roberta_evaluation.py
+++ utils/xml_roberta_evaluation.py
@@ -9,19 +9,17 @@ import torch.nn as nn
 
 def xml_roberta_eval(args, eval_dataset, model, device):
     """ Evaluates the given model on the given dataset. """
-    # f1_metric = evaluate.load("f1")
-    f1_metric = evaluate.load("/jimin/huggingface/modules/evaluate_modules/metrics/evaluate-metric--f1/0ca73f6cf92ef5a268320c697f7b940d1030f8471714bffdb6856c641b818974/f1.py")
-
+    f1_metric = evaluate.load("f1")
+    
     # Note that DistributedSampler samples randomly
     eval_sampler = SequentialSampler(eval_dataset)
     eval_dataloader = DataLoader(
         eval_dataset,
         sampler=eval_sampler,
         batch_size=args.eval_batch_size)
-    # tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
-    tokenizer = AutoTokenizer.from_pretrained("/jimin/huggingface/hub/models--xlm-roberta-base/snapshots/e73636d4f797dec63c3081bb6ed5c7b0bb3f2089", local_files_only=True)
-
-    # Evaluate!
+    tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
+    
+    # Evaluation
     logging.info("***** Running evaluation *****")
     logging.info("  Num examples = %d", len(eval_dataset))
     logging.info("  Batch size = %d", args.eval_batch_size)
@@ -35,25 +33,22 @@ def xml_roberta_eval(args, eval_dataset, model, device):
     cross_entropy_loss = nn.CrossEntropyLoss(ignore_index=-100)
 
     for batch in tqdm.tqdm(eval_dataloader, desc="Evaluating"):
-        # orig_token_inputs = tokenizer(batch[0], add_special_tokens=False, padding='max_length',
-        #                                   truncation=True, max_length=args.max_seq_len, return_tensors="pt").to(device)
         orig_token_inputs = tokenizer(batch[0], padding='max_length', truncation=True,
                                       max_length=args.max_seq_len, return_tensors="pt").to(device)
-        outputs = model(**orig_token_inputs)  # outputs: (b_s, max_len, class_num)
+        outputs = model(**orig_token_inputs)
         outputs = outputs.logits
-        logits = torch.argmax(outputs, dim=2)  # logits: (b_s, max_len)
+        logits = torch.argmax(outputs, dim=2)
 
-        loss = cross_entropy_loss(outputs.flatten(0, 1), batch[2].flatten().to(device))
+        loss = cross_entropy_loss(outputs.flatten(0, 1), batch[1].flatten().to(device))
 
         eval_loss += loss.item()
         nb_eval_steps += 1
         preds = logits.detach().cpu().numpy()
-        # preds = np.argmax(preds, axis=2) # (128, 256)
-        out_label_ids = batch[2].detach().cpu().numpy() # (128, 256)
+        out_label_ids = batch[1].detach().cpu().numpy()
 
         for i in range(out_label_ids.shape[0]):
             for j in range(out_label_ids.shape[1]):
-                if out_label_ids[i, j] != -100:  # 0 is pad_token_id
+                if out_label_ids[i, j] != -100:
                     out_label_list.append(out_label_ids[i, j])
                     preds_list.append(preds[i][j])
 
